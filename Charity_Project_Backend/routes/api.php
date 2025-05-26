1744ffe3c63d1333691a2fe1282e8ef5651d230b
Nour <nourhabian04@gmail.com>
2025-05-26 20:49:07 +0300

    initial apis

--- Charity_Project_Backend/routes/api.php
+++ Charity_Project_Backend/routes/api.php
@@ -15,3 +15,49 @@ Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 Route::put('/editpassword', [UserController::class, 'editPassword'])->middleware('auth:sanctum');
 Route::get('/getUser', [UserController::class, 'GetUserInformation'])->middleware('auth:sanctum');
+
+
+/*
+
+
+
+Route::get('/donor/randomProjects', [ProjectController::class, 'getRandom'])->middleware('auth:sanctum');
+CRUD for projects
+Route::get('/admin/addProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getAllProjects', [ProjectController::class, 'get'])->middleware('auth:sanctum');
+Route::get('/admin/updateProject', [ProjectController::class, 'update'])->middleware('auth:sanctum');
+Route::get('/admin/addProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/admin/getProjectByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/admin/getProjectByType', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/donateToProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/donateToZakat', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/gift', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addMoney', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getNotificationsHistory', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getBalance', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getDonationHistory', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getFav', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getPoints', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getBestDonors', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/searchProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addVolounteer', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getVolounteers', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getVolounteerByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getVolounteerByStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getFeedbacks', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/autoDonation', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/updateDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addFeedback', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getAllBeneficiary', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getBeneficiaryByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/getBeneficiaryByStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addOrRemoveDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/addOrRemoveVolounteerRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/updateProjectStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/acceptFeedbacks', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/banVolounteer', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+Route::get('/donor/banBeneficiary', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+
+
+*/
