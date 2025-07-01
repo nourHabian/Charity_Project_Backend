8119ce8732a98ea0513b4d2a44afc7cbe19b99b6
Nour <nourhabian04@gmail.com>
2025-07-01 19:30:02 +0300

    to do list commented in api page + organizing api page ^^

--- Charity_Project_Backend/app/Http/Controllers/VolunteerController.php
+++ Charity_Project_Backend/app/Http/Controllers/VolunteerController.php
@@ -13,68 +13,66 @@ class VolunteerController extends Controller
 
 
     // التسجيل على استبيان التطوع
-public function addVolunteerRequest(AddVolunteerRequest $request)
-{
-    $user = Auth::user();
+    public function addVolunteerRequest(AddVolunteerRequest $request)
+    {
+        $user = Auth::user();
 
-    if ($user->volunteer_status !== null) {
-        return response()->json([
-            'message' => 'لقد قمت بالتسجيل على استبيان التطوع مسبقًا ولا يمكنك التسجيل مرة أخرى.'
-        ], 400);
+        if ($user->volunteer_status !== null) {
+            return response()->json([
+                'message' => 'لقد قمت بالتسجيل على استبيان التطوع مسبقًا ولا يمكنك التسجيل مرة أخرى.'
+            ], 400);
+        }
+
+        $validatedData = $request->validated();
+        $validatedData['volunteer_status'] = 'معلق';
+
+        $user->update($validatedData);
+
+        $volunteerInfo = $user->only([
+            'full_name',
+            'contact_number',
+
+            'volunteer_status',
+            'purpose_of_volunteering',
+            'current_location',
+            'volunteering_domain',
+            'gender',
+            'age',
+            'volunteering_hours',
+            'education',
+
+        ]);
+
+        return response()->json($volunteerInfo, 201);
     }
 
-    $validatedData = $request->validated();
-    $validatedData['volunteer_status'] = 'معلق';
-
-    $user->update($validatedData);
-
-    $volunteerInfo = $user->only([
-        'full_name',
-        'contact_number',
-
-        'volunteer_status',
-        'purpose_of_volunteering',
-        'current_location',
-        'volunteering_domain',
-        'gender',
-        'age',
-        'volunteering_hours',
-        'education',
-        
-    ]);
-
-    return response()->json($volunteerInfo, 201);
-}
-
-    
 
 
 
-public function getAllVolunteerRequests()
-{
-    $admin = Auth::guard('admin')->user();
 
-    if (!$admin) {
-        return response()->json(['message' => 'غير مصرح لك بالوصول إلى هذه البيانات.'], 403);
+    public function getAllVolunteerRequests()
+    {
+        $admin = Auth::guard('admin')->user();
+
+        if (!$admin) {
+            return response()->json(['message' => 'غير مصرح لك بالوصول إلى هذه البيانات.'], 403);
+        }
+
+        // جلب المستخدمين الذين لديهم استبيانات تطوع
+        $volunteerRequests = User::whereNotNull('volunteer_status')->get([
+            'id',
+            'full_name',
+            'contact_number',
+            'volunteer_status',
+            'volunteering_domain',
+            'purpose_of_volunteering',
+            'current_location',
+            'gender',
+            'age',
+            'volunteering_hours',
+            'education'
+        ]);
+
+        return response()->json($volunteerRequests, 200);
     }
-
-    // جلب المستخدمين الذين لديهم استبيانات تطوع
-    $volunteerRequests = User::whereNotNull('volunteer_status')->get([
-        'id',
-        'full_name',
-        'contact_number',
-        'volunteer_status',
-        'volunteering_domain',
-        'purpose_of_volunteering',
-        'current_location',
-        'gender',
-        'age',
-        'volunteering_hours',
-        'education'
-    ]);
-
-    return response()->json($volunteerRequests, 200);
-}
-
-
 }
--- Charity_Project_Backend/routes/api.php
+++ Charity_Project_Backend/routes/api.php
@@ -16,103 +16,172 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
+
+
+
+// ******************************** DONOR APIS ********************************
+
+// donor account creation
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/verifyEmail', [UserController::class, 'verify_email']);
 Route::post('/login', [UserController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // logout
+    Route::post('/logout', [UserController::class, 'logout']);
+    // edit donor's password
+    Route::put('/editpassword', [UserController::class, 'editPassword']);
+    // get donor's information
+    Route::get('/getUser', [UserController::class, 'GetUserInformation']);
+
+   
+    // ****** DONATE LATER LIST ******
+
+    // add a project to donate later list
+    Route::post('/favourite', [FavouriteController::class, 'addToFavourite']);
+    // remove a project from donate later list
+    Route::delete('/favourite', [FavouriteController::class, 'removeFromFavourite']);
+    // get all projects in donate later list
+    Route::get('/favourite', [FavouriteController::class, 'getFavouriteProjects']);
+    // search in donate later list
+    Route::get('/favourite/search', [FavouriteController::class, 'searchFavourite']);
+
+
+    // ****** VOLUNTEER ACTIONS ******
+
+    // submit a volunteer form
+    Route::post('/donor/volunteerRequest', [VolunteerController::class, 'addVolunteerRequest']);
+    // volunteer in a project
+    Route::post('/volunteer/volunteerInProject/{id}', [UserController::class, 'volunteerInProject']);
+
+
+    // ****** FINANCIAL TRANSACTIONS ******
+
+    // add to balance
+    Route::post('/donor/addToBalance', [UserController::class, 'addToBalance']);
+    // give a gift
+    Route::post('/donor/giveGift', [UserController::class, 'giveGift']);
+    // give zakat
+    Route::post('/donor/giveZakat', [UserController::class, 'giveZakat']);
+    // donate to a project
+    Route::post('/donor/donateToProject/{id}', [UserController::class, 'donateToProject']);
+    // activate monthly donation
+    Route::post('/donor/monthlyDonation', [UserController::class, 'monthlyDonation']);
+    // deactivate monthly donation
+    Route::put('/donor/cancelMonthlyDonation', [UserController::class, 'cancelMonthlyDonation']);
+
+  
+    // ****** PROJECTS VIEWING ******
+
+    // view permanent projects
+    Route::get('/donor/home', [ProjectController::class, 'home']);
+    // view health projects
+    Route::get('/donor/projects/health', [ProjectController::class, 'healthProjects']);
+    // view educational projects
+    Route::get('/donor/projects/educational', [ProjectController::class, 'educationalProjects']);
+    // view residential projects
+    Route::get('/donor/projects/residential', [ProjectController::class, 'residentialProjects']);
+    // view nutritional projects
+    Route::get('/donor/projects/nutritional', [ProjectController::class, 'nutritionalProjects']);
+    // view emergency projects
+    Route::get('/donor/projects/emergency', [ProjectController::class, 'emergencyProjects']);
+    // view volunteer projects filtered by domain
+    Route::get('/getVolunteerProjectsByType/{volunteeringDomain}', [ProjectController::class, 'getVolunteerProjectsByType']);
+
+
+    // ***** VIEWING APP INFORMATION AND PERSONAL HISTORY *****
+
+    // view feedbacks
+    Route::get('/getAcceptedFeedbacks', [FeedbackController::class, 'getAcceptedFeedbacks']);
+    // view top ten donors
+    Route::get('/getTopDonors', [UserController::class, 'getDonorsByPoints']);
+    // view notification history
+    Route::post('/notifications', [NotificationController::class, 'showAllAndMarkAsRead']);
+    // view donation history
+    Route::get('/donations/user', [DonationController::class, 'getUserDonations']);
+    
+
+}); 
+
+
+
+
+// ******************************** ADMIN APIS ********************************
+
+
+// admin account login - logout
 Route::post('/admin/login', [AdminController::class, 'loginAdmin']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
-Route::put('/editpassword', [UserController::class, 'editPassword'])->middleware('auth:sanctum');
-Route::get('/getUser', [UserController::class, 'GetUserInformation'])->middleware('auth:sanctum');
 
-Route::post('/favourite', [FavouriteController::class, 'addToFavourite'])->middleware('auth:sanctum');
-Route::delete('/favourite', [FavouriteController::class, 'removeFromFavourite'])->middleware('auth:sanctum');
-Route::get('/favourite', [FavouriteController::class, 'getFavouriteProjects'])->middleware('auth:sanctum');
-Route::get('/favourite/search', [FavouriteController::class, 'searchFavourite'])->middleware('auth:sanctum');
+Route::middleware('isAdmin')->group(function () {
 
-Route::get('/getVolunteerProjectsByType/{volunteeringDomain}', [ProjectController::class, 'getVolunteerProjectsByType']);
-
-
-Route::get('/getAcceptedFeedbacks', [FeedbackController::class, 'getAcceptedFeedbacks'])->middleware('auth:sanctum');
-
-Route::get('/getTopDonors', [UserController::class, 'getDonorsByPoints'])->middleware('auth:sanctum');
+    Route::post('/admin/doAllMonthlyDonations', [AdminController::class, 'monthlyDonations']);
+    Route::get('/admin/getAllVolunteerRequests', [VolunteerController::class, 'getAllVolunteerRequests']);
+    // Route::post('/admin/addProject', [ProjectController::class, 'addProject']);
+    // Route::put('/admin/editProject/{id}', [ProjectController::class, 'editProject']);
+    Route::delete('/admin/deleteProject/{id}', [ProjectController::class, 'deleteProject']);
+});
 
 
 
-
-Route::post('/donor/volunteerRequest', [VolunteerController::class, 'addVolunteerRequest'])->middleware('auth:sanctum');
-Route::post('/volunteer/volunteerInProject/{id}', [UserController::class, 'volunteerInProject'])->middleware('auth:sanctum');
-
-Route::post('/donor/addToBalance', [UserController::class, 'addToBalance'])->middleware('auth:sanctum');
-Route::post('/donor/giveGift', [UserController::class, 'giveGift'])->middleware('auth:sanctum');
-Route::post('/donor/giveZakat', [UserController::class, 'giveZakat'])->middleware('auth:sanctum');
-Route::post('/donor/donateToProject/{id}', [UserController::class, 'donateToProject'])->middleware('auth:sanctum');
-
-Route::post('/notifications', [NotificationController::class, 'showAllAndMarkAsRead'])->middleware('auth:sanctum');
-Route::get('/donations/user', [DonationController::class, 'getUserDonations'])->middleware('auth:sanctum');
-
-Route::get('/donor/home', [ProjectController::class, 'home'])->middleware('auth:sanctum');
-Route::get('/donor/projects/health', [ProjectController::class, 'healthProjects'])->middleware('auth:sanctum');
-Route::get('/donor/projects/educational', [ProjectController::class, 'educationalProjects'])->middleware('auth:sanctum');
-Route::get('/donor/projects/residential', [ProjectController::class, 'residentialProjects'])->middleware('auth:sanctum');
-Route::get('/donor/projects/nutritional', [ProjectController::class, 'nutritionalProjects'])->middleware('auth:sanctum');
-Route::get('/donor/projects/emergency', [ProjectController::class, 'emergencyProjects'])->middleware('auth:sanctum');
-
-Route::post('/donor/monthlyDonation', [UserController::class, 'monthlyDonation'])->middleware('auth:sanctum');
-Route::put('/donor/cancelMonthlyDonation', [UserController::class, 'cancelMonthlyDonation'])->middleware('auth:sanctum');
-
-
-
-Route::post('/admin/doAllMonthlyDonations', [AdminController::class, 'monthlyDonations']);
-
-
-Route::get('/admin/getAllVolunteerRequests', [VolunteerController::class, 'getAllVolunteerRequests'])->middleware('isAdmin');;
-Route::post('/admin/addProject', [ProjectController::class, 'addProject'])->middleware('isAdmin');
-// Route::put('/admin/editProject/{id}', [ProjectController::class, 'editProject']);
-Route::delete('/admin/deleteProject/{id}', [ProjectController::class, 'deleteProject']);
 /*
+**** TO DO LIST:
+******************** أدمن ***********************
+**** قسم حلا:
+- تسجيل خروج أدمن
+- عرض إحصائيات: (عدد المتطوعين الحالي بالجمعية _بس المقبولين_، عدد المحتاجين، عدد المتبرعين، عدد المشاريع، مبلغ التبرعات الكلية)
+- عرض كل المشاريع مع فلترة حسب نوعها
+- عرض كل طلبات التطوع (مع فلترة مقبول، مرفوض، قيد الدراسة)
+- عرض كل المتطوعين (مع فلترة محظور، مو محظور)
+- عرض كل طلبات المحتاجين (اول شي فلترة حسب صحي تعليمي سكني غذائي وبعدها فلترة مقبول مرفوض قيد الدراسة)
+- عرض كل المحتاجين مع فلترة (محظور - مو محظور)
+- عرض الهدايا الواصلة للمحتاجين (مع فلترة تم التسليم ولم يتم التسليم)
+- عرض الفيدباكات مع فلترة (مرفوض مقبول قيد الدراسة)
+- عرض طلب احتياج كامل مع كل معلوماتو
 
-Route::post('/favourite/{projectId}', [FavouriteController::class, 'addToFavourite'])->middleware('auth:sanctum');
-Route::delete('/favourite/{projectId}', [FavouriteController::class, 'removeFromFavourite'])->middleware('auth:sanctum');
-Route::get('/favourite', [FavouriteController::class, 'getFavouriteProjects'])->middleware('auth:sanctum');
-Route::get('/favourite/search', [FavouriteController::class, 'searchFavourite'])->middleware('auth:sanctum');
 
-Route::get('/donor/randomProjects', [ProjectController::class, 'getRandom'])->middleware('auth:sanctum');
-CRUD for projects
-Route::get('/admin/addProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getAllProjects', [ProjectController::class, 'get'])->middleware('auth:sanctum');
-Route::get('/admin/updateProject', [ProjectController::class, 'update'])->middleware('auth:sanctum');
-Route::get('/admin/addProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/admin/getProjectByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/admin/getProjectByType', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/donateToProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/donateToZakat', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/gift', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addMoney', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getNotificationsHistory', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getBalance', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getDonationHistory', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getFav', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getPoints', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getBestDonors', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/searchProject', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addVolounteer', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getVolounteers', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getVolounteerByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getVolounteerByStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getFeedbacks', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/autoDonation', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/updateDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addFeedback', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getAllBeneficiary', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getBeneficiaryByID', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/getBeneficiaryByStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addOrRemoveDonationRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/addOrRemoveVolounteerRequest', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/updateProjectStatus', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/acceptFeedbacks', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/banVolounteer', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
-Route::get('/donor/banBeneficiary', [ProjectController::class, 'addProject'])->middleware('auth:sanctum');
+
+
+**** قسم نور:
+- حذف مشروع
+- اضافة مشروع (جمعية، تطوع، تبرع)
+- التبرع لاحد المشاريع (تبرع الان)
+- قبول طلب تطوع
+- رفض طلب تطوع
+- حظر متطوع بشرط يكون مقبول قبل
+- قبول محتاج
+- رفض محتاج
+- حظر محتاج
+- زر تم التسليم تبع الهدية
+- قبول فيدباك
+- رفض فيدباك
+- حظر محتاج بسبب فيدباك
+- رفرش الادمن (خالصة)
+
+
+******************** محتاج ***********************
+**** قسم بتول:
+- انشاء حساب محتاج
+- تسجيل دخول 
+- تسجيل خروج
+- تقديم طلب المساعدة
+- ارسال فيدباك بعد ما يكون بعت الطلب
+- يشوف سجل اشعارات
+- تتبع حالة المشاريع
+
+******************** اشعارات المحتاج ***********************
+
+الاشعارات يلي بتوصلو
+- وقت يستلم هدية
+- وقت يبعت طلب مساعدة ويكون قيد الدراسة - ينقبل - ينرفض
+- وقت ينزل المشروع بالتطبيق
+- وقت يتم تسليم الهدية الو
+- وقت يكتمل المشروع
+- وقت 
+
+
+
 
 
 */
